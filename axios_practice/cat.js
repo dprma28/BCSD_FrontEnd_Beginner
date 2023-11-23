27216356e9399f12af28e35e201182fb2106c526
dprma28 <dprma28@gmail.com>
2023-11-23 17:09:35 +0900

    axios practice use async.

--- axios_practice/cat.js
+++ axios_practice/cat.js
@@ -1,12 +1,19 @@
 const cat = document.getElementById('cat');
 
-axios.get('https://api.thecatapi.com/v1/images/search?size=full')
-    .then(res => {
+async function randomCat() {
+    try {
+        const res = await axios.get('https://api.thecatapi.com/v1/images/search?size=full')
         const catUrl = res.data[0].url;
         const catImg = document.createElement('img');
+
         catImg.src = catUrl;
+        catImg.width = 300;
+        catImg.height = 300;
+
         cat.appendChild(catImg);
-    })
-    .catch(error => {
+    } catch(error) {
         console.error('Error: ', error);
-    });
+    }
+}
+
+randomCat();
